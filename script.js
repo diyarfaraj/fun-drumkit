ca7d54dbfccff9d8b11e0055f6cec5cb54f72746
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-04-25 17:19:56 +0200

    laddar upp

---
+++ script.js
@@ -0,0 +1,30 @@
+
+/*const audio = document.querySelector('audio');*/
+
+
+
+	function playSound(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if(!audio) return;
+	audio.currentTime = 0;
+
+	audio.play();
+
+	key.classList.add('playing');
+	
+};
+
+const keys = Array.from(document.querySelectorAll('.keys'));
+
+function removeTransition(e) {  
+	if(e.propertyName !== 'transform') {
+		return;
+	}
+
+	 e.target.classList.remove('playing');
+
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
